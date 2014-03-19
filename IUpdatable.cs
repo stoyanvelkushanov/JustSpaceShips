90db012102fc3de047118824a49d87692c4392ed
Stoyan <stoyan_velkushanov@yahoo.com>
2014-03-19 16:20:33 +0100

    Create IUpdatable.cs

---
+++ IUpdatable.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JustSpaceShips
+{
+    public interface IUpdatable
+    {
+        void UpdateState();
+    }
+}
